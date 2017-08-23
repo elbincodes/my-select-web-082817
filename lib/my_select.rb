95ac1b2892f586597b3ebec587fcff8b757a9aa6
Elbin Keeppanasseril <elbincodes@gmail.com>
2017-08-23 05:15:06 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
  # your code here!
+  if block_given?
+   myArr = []
+   counter = 0
+   while counter < collection.length
+       if yield (collection[counter]) # why doesnt this work if I put == true??
+         myArr.push(collection[counter])
+       end
+     counter += 1
+   end
+   myArr
+  else
+  end
 end
